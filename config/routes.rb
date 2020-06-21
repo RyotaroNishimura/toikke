fbcb228459b52c1ade0ec9733658855b503c6876
西村涼太郎 <nishimuraryoutarou@rnmac.local>
2020-06-20 23:35:02 -0400

    1

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root to: 'static_pages#home'
+  get '/signup', to:'users#new'
+  get '/login', to:'sessions#new'
+  get '/login', to:'sessions#create'
+  delete '/logout', to:'sessions#destroy'
+  resources :users, except: [:new]
+  resources :post
+  resources :microposts, only:[:create, :destroy]
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
