7749c587c165a4052aa471dd4496b6e70ff7a2c9
西村涼太郎 <nishimuraryoutarou@rnmac.local>
2020-06-22 00:15:14 -0400

    '2'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(emil: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user_url(user)
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render 'new'
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,21 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
+  end
+
+  def  create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -15,5 +27,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+    
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :passwrod_confirmation)
   end
 end
