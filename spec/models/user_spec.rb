1d8a3ccbcce4c0773ea568067b8c7aab6871b8f2
西村涼太郎 <nishimuraryoutarou@rnmac.local>
2020-11-11 21:25:55 -0500

    フォロー・フォロワー

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   let(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
 
   context "バリデーション" do
 
@@ -73,4 +74,15 @@ RSpec.describe User, type: :model do
     end
   end
 
+  context "フォロー機能" do
+    it "フォローとアンフォローが正常に動作すること" do
+      expect(user.following?(other_user)).to be_falsey
+      user.follow(other_user)
+      expect(user.following?(other_user)).to be_truthy
+      user.unfollow(other_user)
+      expect(other_user.followed_by?(user)).to be_truthy
+      user.unfollow(other_user)
+      expect(user.following?(other_user)).to be_falsey
+    end
+  end
 end
