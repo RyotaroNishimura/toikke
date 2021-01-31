22b155af11c1e9f4c25949f6440f85d6498140e6
西村涼太郎 <nishimuraryoutarou@rnmac.local>
2021-01-31 18:24:11 +0900

    お気に入り画面の編集

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,5 @@ class StaticPagesController < ApplicationController
       @post = current_user.posts.build
       @feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
     end
-
-
   end
 end
