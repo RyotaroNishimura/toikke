d0e14f91ade126b47aee31f247440c8de4596b18
西村涼太郎 <nishimuraryoutarou@rnmac.local>
2021-02-01 23:52:01 +0900

    お知らせ機能の再構築

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @post = current_user.posts.build
+      @user = User.all.build
+      @post = Post.all.build
       @feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
     end
   end
