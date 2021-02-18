084c119e143929aba21646d3669c4928ea99422d
Ryotaro Nishimura <baseball.1231012181@gmail.com>
2021-02-18 12:46:04 +0900

    carrier_waveの修正

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,6 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.fog_credentials = {
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -13,7 +12,6 @@ CarrierWave.configure do |config|
     region: ENV['S3_REGION'],
     aws_access_key_id: ENV['S3_ACCESS_KEY'],
     aws_secret_access_key: ENV['S3_SECRET_KEY']
-    bucket: 'toikke'
   }
   config.fog_directory     =  ENV['S3_BUCKET']
 end
