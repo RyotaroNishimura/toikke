5e493dff585fb583e67fd0ebfd5a8ffec151d340
Ryotaro Nishimura <baseball.1231012181@gmail.com>
2021-02-19 14:18:03 +0900

    carrier_waveの変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -12,11 +12,10 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
 
       provider: 'AWS',
-      region: ENV['S3_REGION'],
-      aws_access_key_id: ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY']
+      region: ENV['AWS_DEFAULT_REGION'],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
-    config.fog_directory     = ENV['S3_BUCKET']
   else
     config.storage :file
   end
