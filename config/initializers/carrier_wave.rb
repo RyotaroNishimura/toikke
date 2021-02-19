736dc95723ca138af212ce6be584bc02e40e303c
Ryotaro Nishimura <baseball.1231012181@gmail.com>
2021-02-20 00:54:47 +0900

    環境変数の変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -12,9 +12,9 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
 
       provider: 'AWS',
-      region: ENV['AWS_DEFAULT_REGION'],
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      region: 'ap-northeast-1',
+      aws_access_key_id: 'AWS_ACCESS_KEY_ID',
+      aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY'
     }
   else
     config.storage :file
