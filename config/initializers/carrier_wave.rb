5717569c0c4a4a0ec5cd14b5314369dc08f826bc
Ryotaro Nishimura <baseball.1231012181@gmail.com>
2021-02-19 17:59:15 +0900

    bucketnameの変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = '[toikke]'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/[toikke]'
+    config.fog_directory  = 'toikke'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/toikke'
     config.fog_public = false
     config.fog_credentials = {
 
