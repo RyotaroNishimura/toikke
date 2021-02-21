e349bff7f9f0f432c10205da3a08e2525e5dea91
Ryotaro Nishimura <baseball.1231012181@gmail.com>
2021-02-21 19:17:24 +0900

    空行削除

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,9 +7,9 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'toikke'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/toikke'
     config.fog_public = false
     config.fog_credentials = {
-
       provider: 'AWS',
       region: 'ap-northeast-1',
       aws_access_key_id: 'AKIAYJZR3NNC6JZ64FXG',
