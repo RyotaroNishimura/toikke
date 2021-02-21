acf5d54f7e33961e40745f21865eaa009788b0ec
Ryotaro Nishimura <baseball.1231012181@gmail.com>
2021-02-21 22:45:11 +0900

    環境ごとの分岐の削除

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'toikke'
@@ -15,9 +14,5 @@ CarrierWave.configure do |config|
       aws_access_key_id: 'AKIAYJZR3NNC6JZ64FXG',
       aws_secret_access_key: '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'
     }
-  else
-    config.storage :file
-  end
-
 end
 
