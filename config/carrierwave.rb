4d47c34a5a668560ac42fb02669b37c89647691a
Ryotaro Nishimura <baseball.1231012181@gmail.com>
2021-02-23 19:53:49 +0900

    carrierwaveの変更

---
+++ config/carrierwave.rb
@@ -0,0 +1,20 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'toikkern'
+    config.asset_host = 'https://s3.amazonaws.com/toikkern'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1',
+    }
+  else
+    config.storage :file
+  end
+end
